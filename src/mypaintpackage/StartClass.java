5ab4a4f30413da80f208baa87cb498347fb18ddd
MaximShishkin <yacyfra@gmail.com>
2022-03-11 21:01:32 +0300

    first commit

---
+++ src/mypaintpackage/StartClass.java
@@ -0,0 +1,10 @@
+package mypaintpackage;
+
+public class StartClass {
+
+	// Главный метод, который запускает программу
+	public static void main(String[] args) {
+		// создаём объект окна
+		new FrameClass();
+	}
+}
